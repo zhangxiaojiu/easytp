0bdd504ae67138e12e0f1d192971f51c89ec7810
Mr.xiaojiu Win <274276153@qq.com>
2017-11-23 23:10:08 +0800

    文章排序 Signed-off-by: Mr.xiaojiu Win <274276153@qq.com>

--- App/Modules/Home/Action/IndexAction.class.php
+++ App/Modules/Home/Action/IndexAction.class.php
@@ -3,7 +3,7 @@
 Class IndexAction extends SystemAction{
 
 	Public function index(){
-		$this-> lists = M('news')->where(array('cid'=>1))->select();
+		$this-> lists = M('news')->where(array('cid'=>1))->order('uptime desc')->select();
         $nid = M('nav')->where(['uid'=>1,'state'=>1])->find();
         $nav = M('navList')->where(['nid'=>$nid['id']])->order('sort')->select();
         $footContent = M('news')->where(['lid'=>1,'title'=>'底部说明'])->find()['content'];
