d69712ba04e21aa196b960eb24b24d5fa236ccfe
Mac.xiaojiu <274276153@qq.com>
2017-11-25 18:23:24 +0800

    9
    网站公告

--- App/Modules/Home/Action/IndexAction.class.php
+++ App/Modules/Home/Action/IndexAction.class.php
@@ -7,7 +7,9 @@ Class IndexAction extends SystemAction{
         $nid = M('nav')->where(['uid'=>1,'state'=>1])->find();
         $nav = M('navList')->where(['nid'=>$nid['id']])->order('sort')->select();
         $footContent = M('news')->where(['lid'=>1,'title'=>'底部说明'])->find()['content'];
+        $notice = M('news')->where(['lid'=>1,'title'=>'网站公告'])->find()['content'];
         $this->assign('footContent', $footContent);
+        $this->assign('notice', $notice);
         $this->assign('nav',$nav);
 		$this->display();
 	}
