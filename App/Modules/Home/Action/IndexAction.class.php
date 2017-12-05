c24eab0f749b0ba6825e52efe4394299562ab392
Mr.xiaojiu Win <274276153@qq.com>
2017-12-05 17:52:30 +0800

    Signed-off-by: Mr.xiaojiu Win <274276153@qq.com>

--- App/Modules/Home/Action/IndexAction.class.php
+++ App/Modules/Home/Action/IndexAction.class.php
@@ -4,6 +4,13 @@ Class IndexAction extends SystemAction{
 
 	Public function index(){
 		$this-> lists = M('news')->where(array('cid'=>1))->order('uptime desc')->select();
+        $nid = M('nav')->where(['uid'=>1,'state'=>1])->find();
+        $nav = M('navList')->where(['nid'=>$nid['id']])->order('sort')->select();
+        $footContent = M('news')->where(['lid'=>1,'title'=>'底部说明'])->find()['content'];
+        $notice = M('news')->where(['lid'=>1,'title'=>'网站公告'])->find()['content'];
+        $this->assign('nav',$nav);
+        $this->assign('footContent', $footContent);
+        $this->assign('notice', $notice);
 		$this->display();
 	}
 	Public function about(){
